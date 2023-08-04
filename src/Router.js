af1a176dc456bd7e25e05e982d912ed53b74451a
Collins <collinskiarie13@outlook.com>
2023-08-04 07:00:40 +0300

    Added routes for the Sales, Products and Staff components

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import NavBar from './NavBar';
+import Sales from './Sales'
+import Products from './Products'
+import Staff from './Staff'
 
 function Router({handleLogin, handleLogout, loggedInUser, loginError}) {
   return (
@@ -11,6 +14,15 @@ function Router({handleLogin, handleLogout, loggedInUser, loginError}) {
       
       {/* Route for the NavBar component */}
       <Route path="/navbar" element={<NavBar user={loggedInUser} handleLogout={handleLogout}/>} />
+
+         {/* Route for the Sales component */}
+         <Route path="/sales" element={<Sales />} />
+
+            {/* Route for the Products component */}
+      <Route path="/products" element={<Products />} />
+
+         {/* Route for the Staff component */}
+        <Route path="/staff" element={<Staff  />} />
     </Routes>
   );
 }
